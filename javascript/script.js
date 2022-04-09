ace33a5ddcfa4f108d47097389fd0bd974ebba2d
Casey MacLeod <caseymacleod22@gmail.com>
2022-04-09 01:11:09 -0400

    console logs dice rolls

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+function rollDie1() {
+console.log(Math.floor(Math.random() *7))
+}
+function rollDie2() {
+console.log(Math.floor(Math.random() *7))
+}
+function rollDie3() {
+console.log(Math.floor(Math.random() *7))
+}
+function rollDie4() {
+console.log(Math.floor(Math.random() *7))
+}
+function rollDie5() {
+console.log(Math.floor(Math.random() *7))
+}
+
+rollDie1()
+rollDie2()
+rollDie3()
+rollDie4()
+rollDie5()
