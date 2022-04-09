c0277dd8669cdeaf24bd782780e2ba6f9628c0ae
Casey MacLeod <caseymacleod22@gmail.com>
2022-04-09 01:28:45 -0400

    console logs 5 dice on click of button

--- javascript/script.js
+++ javascript/script.js
@@ -1,26 +1,27 @@
 function rollDie1() {
-const result = (Math.floor(Math.random() *7))
-return result
-}
-function rollDie2() {
-const result = (Math.floor(Math.random() *7))
-return result
-}
-function rollDie3() {
-const result = (Math.floor(Math.random() *7))
-return result
-}
-function rollDie4() {
-const result = (Math.floor(Math.random() *7))
-return result
-}
-function rollDie5() {
-const result = (Math.floor(Math.random() *7))
-return result
+const result1 = (Math.floor(Math.random() *7))
+console.log(result1)
 }
 
-rollDie1()
-rollDie2()
-rollDie3()
-rollDie4()
-rollDie5()
+function rollDie2() {
+const result2 = (Math.floor(Math.random() *7))
+console.log(result2)
+}
+function rollDie3() {
+const result3 = (Math.floor(Math.random() *7))
+console.log(result3)
+}
+function rollDie4() {
+const result4 = (Math.floor(Math.random() *7))
+console.log(result4)
+}
+function rollDie5() {
+const result5 = (Math.floor(Math.random() *7))
+console.log(result5)
+}
+
+// rollDie1()
+// rollDie2()
+// rollDie3()
+// rollDie4()
+// rollDie5()
