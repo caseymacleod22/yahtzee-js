c9522099a920b5b22e76dee0d2cb97cadf8c7975
Casey MacLeod <caseymacleod22@gmail.com>
2022-04-11 22:57:40 -0400

    shows die resul on screen for die

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ console.log(result1)
 
 function rollDie2() {
 const result2 = (Math.floor(Math.random() *7))
-console.log(result2)
+return document.getElementById('die2').textContent = result2;
 }
 function rollDie3() {
 const result3 = (Math.floor(Math.random() *7))
