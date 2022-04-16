713f1935cf1126968f4e080db54611e0269d9396
Casey MacLeod <caseymacleod22@gmail.com>
2022-04-16 00:00:36 -0400

    adds dice function

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,50 @@
-function rollDie1() {
-const result1 = (Math.floor(Math.random() *7))
-return document.getElementById('die1').textContent = result1;
+const dice = new array (0, 0, 0, 0, 0)
+const hold = new array (0, 0, 0, 0, 0)
+let turn = 0  
+
+function rollDice() {
+    if (turn < 3) {
+        for (i = 0; i < 6; i++) {
+            if (hold[i] === 0) {
+                dice[i] = Math.floor(Math.random() * 6) + 1;
+                console.log(dice)
+            }
+        }
+        turn = ++turn
+    }
+    else {
+        console.log('aint work fam')
+    }
 }
 
-function rollDie2() {
-const result2 = (Math.floor(Math.random() *7))
-return document.getElementById('die2').textContent = result2;
-}
-function rollDie3() {
-const result3 = (Math.floor(Math.random() *7))
-return document.getElementById('die3').textContent = result3;
-}
-function rollDie4() {
-const result4 = (Math.floor(Math.random() *7))
-return document.getElementById('die4').textContent = result4;
-}
-function rollDie5() {
-const result5 = (Math.floor(Math.random() *7))
-return document.getElementById('die5').textContent = result5;
-}
+rollDice()
 
-function rollDice () {
-    return rollDie1(), rollDie2(), rollDie3(), rollDie4(), rollDie5()
-}
+
+// function rollDie1() {
+// const result1 = (Math.floor(Math.random() *7))
+// return document.getElementById('die1').textContent = result1;
+// }
+
+// function rollDie2() {
+// const result2 = (Math.floor(Math.random() *7))
+// return document.getElementById('die2').textContent = result2;
+// }
+// function rollDie3() {
+// const result3 = (Math.floor(Math.random() *7))
+// return document.getElementById('die3').textContent = result3;
+// }
+// function rollDie4() {
+// const result4 = (Math.floor(Math.random() *7))
+// return document.getElementById('die4').textContent = result4;
+// }
+// function rollDie5() {
+// const result5 = (Math.floor(Math.random() *7))
+// return document.getElementById('die5').textContent = result5;
+// }
+
+// function rollDice () {
+//     return rollDie1(), rollDie2(), rollDie3(), rollDie4(), rollDie5()
+// }
 
 // rollDie1()
 // rollDie2()
