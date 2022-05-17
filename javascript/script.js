2c2683ce2bfc52f18b47975f225835cec32a70d1
Casey MacLeod <caseymacleod22@gmail.com>
2022-05-16 22:44:52 -0400

    removes extra console logs

--- javascript/script.js
+++ javascript/script.js
@@ -14,7 +14,6 @@ function rollDie() {
     let roll4 = Math.floor(Math.random() * 6) + 1;
     let roll5 = Math.floor(Math.random() * 6) + 1;
     
-    console.log(roll1, roll2, roll3, roll4, roll5)
     return document.getElementById('die1').textContent = roll1, document.getElementById('die2').textContent = roll2, document.getElementById('die3').textContent = roll3, document.getElementById('die4').textContent = roll4, document.getElementById('die5').textContent = roll5
 }}
 
