0192db704032640ec90cd77d71049dfb7a6f5b08
Casey MacLeod <caseymacleod22@gmail.com>
2022-05-17 23:13:48 -0400

    wip: if class is active, dont reroll die

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,15 @@
 let held = []
 
 function rollDie() {
-    if ($(".dice-roll").hasClass("active")) {
-        let roll1;
-        let roll2;
-        let roll3;
-        let roll4;
-        let roll5;
-    } else {
     let roll1 = Math.floor(Math.random() * 6) + 1;
     let roll2 = Math.floor(Math.random() * 6) + 1;
     let roll3 = Math.floor(Math.random() * 6) + 1;
     let roll4 = Math.floor(Math.random() * 6) + 1;
     let roll5 = Math.floor(Math.random() * 6) + 1;
-    
+
+    if ($("#die1").hasClass("active")) {
+        return roll1 = $('die1').text(), document.getElementById('die2').textContent = roll2, document.getElementById('die3').textContent = roll3, document.getElementById('die4').textContent = roll4, document.getElementById('die5').textContent = roll5
+    } else {
     return document.getElementById('die1').textContent = roll1, document.getElementById('die2').textContent = roll2, document.getElementById('die3').textContent = roll3, document.getElementById('die4').textContent = roll4, document.getElementById('die5').textContent = roll5
 }}
 
