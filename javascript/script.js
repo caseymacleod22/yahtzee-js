7c3470a68df816b3e9aedb2d12f304734e1cf1a1
Casey MacLeod <caseymacleod22@gmail.com>
2022-05-17 23:51:33 -0400

    wip: hold dice when class is active

--- javascript/script.js
+++ javascript/script.js
@@ -7,11 +7,14 @@ function rollDie() {
     let roll4 = Math.floor(Math.random() * 6) + 1;
     let roll5 = Math.floor(Math.random() * 6) + 1;
 
-    if ($("#die1").hasClass("active")) {
-        return roll1 = $('die1').text(), document.getElementById('die2').textContent = roll2, document.getElementById('die3').textContent = roll3, document.getElementById('die4').textContent = roll4, document.getElementById('die5').textContent = roll5
-    } else {
-    return document.getElementById('die1').textContent = roll1, document.getElementById('die2').textContent = roll2, document.getElementById('die3').textContent = roll3, document.getElementById('die4').textContent = roll4, document.getElementById('die5').textContent = roll5
-}}
+    if ($(".dice-roll").hasClass("active")) {
+        return $('.dice-roll').text()
+} else {
+        return document.getElementById('die1').textContent = roll1, document.getElementById('die2').textContent = roll2, document.getElementById('die3').textContent = roll3, document.getElementById('die4').textContent = roll4, document.getElementById('die5').textContent = roll5
+    }   
+}     
+
+
 
 function throwDice() {
     rollDie()   
