2da255cbae781d2855c4093a6d52491641c6e2d8
Casey MacLeod <caseymacleod22@gmail.com>
2022-05-22 22:22:39 -0400

    wip: active class function

--- javascript/script.js
+++ javascript/script.js
@@ -8,7 +8,7 @@ function rollDie() {
     let roll5 = Math.floor(Math.random() * 6) + 1;
 
     if ($(".dice-roll").hasClass("active")) {
-        return roll1 = $('.die1').text(), roll2 = $('.die2').text(), roll3 = $('.die3').text(), roll4 = $('.die4').text(), roll5 = $('.die5').text()
+        return roll1 = $('.die1').text(), document.getElementById('die2').textContent = roll2, document.getElementById('die3').textContent = roll3, document.getElementById('die4').textContent = roll4, document.getElementById('die5').textContent = roll5
     } else {
     return document.getElementById('die1').textContent = roll1, document.getElementById('die2').textContent = roll2, document.getElementById('die3').textContent = roll3, document.getElementById('die4').textContent = roll4, document.getElementById('die5').textContent = roll5
 }}
